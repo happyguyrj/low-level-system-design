19da25f5d8c2ee8268d5af38cda5bdf507b47b62
Rahul Jain <jainrahu@arcesium.com>
2022-02-04 22:52:36 +0530

    Add comments

--- sorting/src/main/java/com/example/app/Sort.java
+++ sorting/src/main/java/com/example/app/Sort.java
@@ -2,5 +2,7 @@ package com.example.app;
 
 public interface Sort<E> {
 
+    // to assure Java that we are passing it a comparator that is defined on any supertype of E
+    //• We do so by giving a lower bound for the argument.
     E[] sort(E[] list, Comparator<? super E> comparator);
 }
