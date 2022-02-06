e630d30ce3e737d6cab3947ddf663af6ad6a532b
Rahul Jain <jainrahu@arcesium.com>
2022-02-06 22:55:44 +0530

    Adding rest controller

---
+++ Rest/productRepo/src/main/java/com/example/productRepo/ProductRepoController.java
@@ -0,0 +1,8 @@
+package com.example.productRepo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//The @RestController annotation is used to define the RESTful web services. It serves JSON, XML and custom response
+public class ProductRepoController {
+}
